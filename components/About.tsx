e69479ac0950c5d937769b01c4596d260643007d
warriorf2555 <faiqfisol@airasia.com>
2023-03-04 21:29:15 +0800

    FEATURE: [Hero] Profile Section

---
+++ components/About.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function About({}: Props) {
+  return <div className="flex flex-col relative h-screen text-center md:text-left">About</div>;
+}
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,27 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import About from "../components/About";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-slate-800 text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Faiq&apos;s portfolio</title>
       </Head>
 
-      {/* Header */}
+      <Header />
 
       {/* Hero */}
+      <section id="hero" className="snap-start">
+        <Hero />
+      </section>
 
       {/* About */}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
       {/* Experience */}
 
@@ -21,8 +30,6 @@ const Home: NextPage = () => {
       {/* Projects */}
 
       {/* Contact me */}
-
-      <h1 className="text-red-500">Lets&apos;s Build an AWESOME portfolio</h1>
     </div>
   );
 };
